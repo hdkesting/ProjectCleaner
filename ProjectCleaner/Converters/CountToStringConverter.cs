ef26373a318c276ec2d06bdd34a4cc934e597932
Hans Kesting <hans_kesting@hotmail.com>
2023-12-29 16:37:22 +0100

    show "busy" as "..."

---
+++ ProjectCleaner/Converters/CountToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace SolutionCleaner.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+internal class CountToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var count = value as int?;
+        if (count.HasValue)
+        {
+            return count.Value.ToString("N0");
+        }
+
+        return "…";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
